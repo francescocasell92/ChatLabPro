5029694d87e6a1cd0716606ebad09aeef09a8585
francescocasell92 <78543601+francescocasell92@users.noreply.github.com>
2021-04-22 15:23:14 +0200

    main User 1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "User.h"
+#include "iostream"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    User tom("tom","1234","firenze");
+
+    std::cout << "Hello my name is "+ tom.getName()+" and my number is:"+ tom.getNumber()+", i am from "+ tom.getCity()<<std::endl;
+
     return 0;
 }
