959ea7fd22bad6e040aadc0a33232532523bf10b
francescocasell92 <78543601+francescocasell92@users.noreply.github.com>
2021-04-22 12:49:35 +0200

    first commit

---
+++ Observer.h
@@ -0,0 +1,19 @@
+//
+// Created by francesco caselli on 22/04/21.
+//
+
+#ifndef CHATLABPRO_OBSERVER_H
+#define CHATLABPRO_OBSERVER_H
+
+#include "memory"
+
+class Observer {
+public:
+    virtual ~Observer(){}
+    virtual void attach() = 0;
+    virtual void detach() = 0;
+    virtual void update() = 0;
+
+};
+
+#endif //CHATLABPRO_OBSERVER_H
