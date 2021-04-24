80e7b6f81dbc40dd0c021b7d2895c4340e3eeffb
francescocasell92 <78543601+francescocasell92@users.noreply.github.com>
2021-04-24 16:15:25 +0200

    1.2

---
+++ cmake-build-debug/test/CMakeFiles/runAllTests.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runAllTests.dir/ChatTest.cpp.o"
+  "CMakeFiles/runAllTests.dir/MssageTest.cpp.o"
+  "CMakeFiles/runAllTests.dir/UserTest.cpp.o"
+  "CMakeFiles/runAllTests.dir/runAllTests.cpp.o"
+  "runAllTests"
+  "runAllTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runAllTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/test/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/francescocaselli1992/Documents/ChatLabPro/test/lib/googletest/googletest
+# Build directory: /Users/francescocaselli1992/Documents/ChatLabPro/cmake-build-debug/test/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
