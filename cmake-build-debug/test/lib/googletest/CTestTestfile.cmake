e35d6074d9a9b56432fa2b9c71c274e820299f3d
francescocasell92 <78543601+francescocasell92@users.noreply.github.com>
2021-06-20 14:29:37 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/test/lib/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/francescocaselli1992/Documents/ChatLabPro/test/lib/googletest
-# Build directory: /Users/francescocaselli1992/Documents/ChatLabPro/cmake-build-debug/test/lib/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
